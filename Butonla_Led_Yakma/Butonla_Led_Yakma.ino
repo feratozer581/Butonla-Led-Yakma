ca9c88e1a9825719780845b00bc7642495db566b
Arduino <feratozer581@gmail.com>
2017-03-11 10:01:30 +0200

    Add files via upload

---
+++ Butonla_Led_Yakma/Butonla_Led_Yakma.ino
@@ -0,0 +1,20 @@
+int buton = 5; // 5. pine butonumuzu bağladık.
+int led = 8; // 8. pine ledimizi bağladık.
+
+void setup(){
+
+pinMode(led, OUTPUT);
+pinMode(buton,INPUT);
+}
+
+void loop(){
+digitalRead(buton, HIGH);
+digitalWrite(led, HIGH);
+
+if{buton=1, digitalWrite(led, HIGH);} // eğer butona basılırsa ledi yak.
+
+else{digitalWrite(led, LOW);
+}
+}
+
+
